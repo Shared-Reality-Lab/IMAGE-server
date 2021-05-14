2e031c533afd552e8c9b3d5e557aee201f287476
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-05-14 17:39:28 -0400

    Add functions to get labelled docker services
    This should form the basis of preprocessor and handler communications.

---
+++ docker/receiver/src/docker.ts
@@ -0,0 +1,27 @@
+import Docker from "dockerode";
+
+export const docker = new Docker();
+const _PREPROCESSOR_LABEL_ = "ca.mcgill.cim.bach.atp.preprocessor";
+const _HANDLER_LABEL_ = "ca.mcgill.cim.bach.atp.handler";
+
+export async function getPreprocessorServices(containers: Docker.ContainerInfo[]) {
+    const activePreprocessors = containers.filter(container => {
+        return (container.State === "running") && (container.Labels[_PREPROCESSOR_LABEL_]);
+    });
+    return activePreprocessors.sort((first, second) => {
+        const firstNum = Number(first.Labels[_PREPROCESSOR_LABEL_]);
+        const secondNum = Number(second.Labels[_PREPROCESSOR_LABEL_]);
+        return firstNum - secondNum;
+    }).map(container => { return container.Labels["com.docker.compose.service"]; });
+}
+
+export async function getHandlerServices(containers: Docker.ContainerInfo[]) {
+    const activeHandlers = containers.filter(container => {
+        return (container.State === "running") && (container.Labels[_HANDLER_LABEL_]);
+    });
+    return activeHandlers.sort((first, second) => {
+        const firstNum = Number(first.Labels[_HANDLER_LABEL_]);
+        const secondNum = Number(second.Labels[_HANDLER_LABEL_]);
+        return firstNum - secondNum;
+    }).map(container => { return container.Labels["com.docker.compose.service"]; });
+}
