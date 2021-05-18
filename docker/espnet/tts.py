f383cdc86b18d97effce11fc8f8aef43f5482605
Juliette Regimbal <juliette.regimbal@mail.mcgill.ca>
2021-05-18 08:06:27 -0400

    Working tts function

---
+++ docker/espnet/tts.py
@@ -0,0 +1,33 @@
+import torch
+import time
+from espnet_model_zoo.downloader import ModelDownloader
+from espnet2.bin.tts_inference import Text2Speech
+from parallel_wavegan.utils import download_pretrained_model
+from parallel_wavegan.utils import load_model
+
+device = "cpu"
+
+def tts(text, tag = "kan-bayashi/ljspeech_conformer_fastspeech2", vocoder_tag = "ljspeech_parallel_wavegan.v1"):
+    d = ModelDownloader()
+    text2speech = Text2Speech(
+            **d.download_and_unpack(tag),
+            device=device,
+            threshold=0.5,
+            minlenratio=0.0,
+            maxlenratio=10.0,
+            use_att_constraint=False,
+            backward_window=1,
+            forward_window=3,
+            # Only for FastSpeech & FastSpeech2
+            speed_control_alpha=1.0,
+    )
+    text2speech.spc2wav = None
+    vocoder = load_model(download_pretrained_model(vocoder_tag)).to(device).eval()
+    vocoder.remove_weight_norm()
+
+    with torch.no_grad():
+        start = time.time()
+        wav, c, *_ = text2speech(text)
+        wav = vocoder.inference(c)
+    #rtf = (time.time() - start) / (len(wav) / fs)
+    return wav.view(-1).cpu().numpy(), text2speech.fs
