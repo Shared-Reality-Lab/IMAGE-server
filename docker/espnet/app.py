d7aaff577c185e49bd2e9b18c9370655571e71ff
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-05-18 10:50:46 -0400

    Basic request handle

--- docker/espnet/app.py
+++ docker/espnet/app.py
@@ -1,7 +1,7 @@
 import logging
 import soundfile as sf
 from espnet_util import tts, fs
-from flask import Flask, Response
+from flask import Flask, Response, request
 from io import BytesIO
 from werkzeug.wsgi import FileWrapper
 
@@ -11,11 +11,16 @@ logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
-@app.route("/service/default-tts", methods=["GET"])
+@app.route("/service/default-tts", methods=["POST"])
 def perform_tts():
-    default_string = "Penny is a very circular cat."
+    data = request.get_json()
+    if data is None or "text" not in data:
+        return { "error": "Missing key \"text\"." }, 400
+    elif not isinstance(data["text"], str):
+        return { "error": "Key \"text\" must be of type string." }, 400
+    text = data["text"]
     try:
-        wav = tts(default_string)
+        wav = tts(text)
         f = BytesIO()
         sf.write(f, wav, fs, format="WAV")
         f.seek(0)
