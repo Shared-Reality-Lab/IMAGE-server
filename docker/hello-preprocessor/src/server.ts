54dae6d312466c57525ecc1b546e1a24ce678795
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-05-19 18:50:15 -0400

    Hello preprocessor server written

--- docker/hello-preprocessor/src/server.ts
+++ docker/hello-preprocessor/src/server.ts
@@ -1,107 +1,42 @@
 import express from "express";
 import Ajv from "ajv/dist/2020";
 
-import querySchemaJSON from "./query.schema.json";
-import { docker, getPreprocessorServices, getHandlerServices } from "./docker";
+import querySchemaJSON from "./schemas/request.schema.json";
+import preprocessorResponseSchemaJSON from "./schemas/preprocessor-response.schema.json";
+import responseSchemaJSON from "./schemas/response.schema.json";
 
 const app = express();
-const port = 8080;
+const port = 80;
 const ajv = new Ajv({
-    "schemas": [querySchemaJSON]
+    "schemas": [querySchemaJSON, responseSchemaJSON, preprocessorResponseSchemaJSON]
 });
-
-const PREPROCESSOR_TIME_MS = 15000;
 
 app.use(express.json());
 
-app.post("/atp/render", (req, res) => {
-    if (ajv.validate("https://bach.cim.mcgill.ca/atp/query.schema.json", req.body)) {
-        // get list of preprocessors and handlers
-        docker.listContainers().then(async (containers) => {
-            const preprocessors = getPreprocessorServices(containers);
-            const handlers = getHandlerServices(containers);
-
-            // TODO do things with these services
-            // Preprocessors run in order
-            const data = req.body;
-            for (const preprocessor of preprocessors) {
-                const controller = new AbortController();
-                const timeout = setTimeout(() => {
-                    controller.abort();
-                }, PREPROCESSOR_TIME_MS);
-
-                await fetch(`http://${preprocessor}/atp/preprocessor`, {
-                    "method": "POST",
-                    "headers": {
-                        "Content-Type": "application/json"
-                    },
-                    "signal": controller.signal
-                }).then(resp => {
-                    return resp.json();
-                }).then(json => {
-                    data.preprocessors[preprocessor] = json;
-                }).catch(err => {
-                    // Try to continue...
-                    // tslint:disable-next-line:no-console
-                    console.error(err);
-                });
+app.post("/atp/preprocessor", (req, res) => {
+    if (ajv.validate("https://bach.cim.mcgill.ca/atp/request.schema.json", req.body)) {
+        // tslint:disable-next-line:no-console
+        console.debug("Request validated");
+        const response = {
+            "request_uuid": req.body.request_uuid,
+            "timestamp": Math.round(Date.now() / 1000),
+            "name": "ca.mcgill.cim.bach.atp.hello.preprocessor",
+            "data": {
+                "message": "Hello, World!"
             }
-
-            // in docker, the service names are resolved and balanced automatically
-        }).then(() => {
-            res.json(
-                {
-                    "request_uuid": req.body.request_uuid,
-                    "timestamp": Math.round(Date.now() / 1000),
-                    "renderings": [
-                      {
-                        "metadata": {
-                        "type_id": "697db3a5-2474-4b34-b203-670af34943bc",
-                          "confidence": 85,
-                          "creator_rendering": {
-                            "metadata": {
-                              "type_id": "697db3a5-2474-4b34-b203-670af34943bc",
-                              "confidence": 100,
-                              "description": null,
-                              "creator_url": "https://srl.mcgill.ca/atp",
-                              "more_details_rendering": null
-                            },
-                            "text_string": "Created by the McGill University SRL team"
-                          },
-                          "creator_url": "https://srl.mcgill.ca/atp/",
-                          "more_details_rendering": {
-                            "metadata": {
-                              "type_id": "c640f825-6192-44ce-b1e4-dd52e6ce6c63",
-                              "confidence": 73,
-                              "description": "Activate to hear about this picture in 3D audio on your headphones.",
-                              "creator_rendering": {
-                                "metadata": {
-                                  "type_id": "697db3a5-2474-4b34-b203-670af34943bc",
-                                  "confidence": 100,
-                                  "creator_url": "https://srl.mcgill.ca/atp/",
-                                  "description": null,
-                                  "more_details_rendering": null
-                                },
-                                "text_string": "Created by the McGill University SRL team"
-                              }
-                            },
-                            "haptic_url": null,
-                            "audio_url": "https://bach.cim.mcgill.ca/atp/testpages/tp01/test01.mp3",
-                            "media_tags": null,
-                            "more_details_rendering": null
-                          }
-                        },
-                        "text_string": "Picture of a Pebble smartwatch strapped to a hairy leg outdoors."
-                      }
-                    ]
-                  }
-            );
-        }).catch(e => {
+        };
+        if (ajv.validate("https://bach.cim.mcgill.ca/atp/preprocessor-response.schema.json", response)) {
             // tslint:disable-next-line:no-console
-            console.error(e);
-            res.status(500).send(e.name + ": " + e.message);
-        });
+            console.debug("Valid response generated.");
+            res.json(response);
+        } else {
+            // tslint:disable-next-line:no-console
+            console.debug("Failed to generate a valid response (did the schema change?)");
+            res.status(500).send(ajv.errors);
+        }
     } else {
+        // tslint:disable-next-line:no-console
+        console.debug("Request did not pass the schema.");
         res.status(400).send(ajv.errors);
     }
 });
