00eb66a173cba9641e68c5f37cdd1a80fdc6cf1e
rohanakut <rohan.akut97@gmail.com>
2021-05-20 18:52:37 -0400

    Added ML code

--- docker/espnet/src/predownload.py
+++
@@ -1,6 +0,0 @@
-from espnet_model_zoo.downloader import ModelDownloader
-from parallel_wavegan.utils import download_pretrained_model
-
-d = ModelDownloader()
-d.download_and_unpack("kan-bayashi/ljspeech_conformer_fastspeech2")
-download_pretrained_model("ljspeech_parallel_wavegan.v1")
