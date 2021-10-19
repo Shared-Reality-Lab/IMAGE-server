6e4f1b3156e791f0fc69c62dd655a9bbff29615f
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-10-19 14:41:18 -0400

    Move from tslint to eslint

--- preprocessors/hello-preprocessor/src/server.ts
+++ preprocessors/hello-preprocessor/src/server.ts
@@ -15,7 +15,6 @@ app.use(express.json({limit: process.env.MAX_BODY}));
 
 app.post("/preprocessor", (req, res) => {
     if (ajv.validate("https://image.a11y.mcgill.ca/request.schema.json", req.body)) {
-        // tslint:disable-next-line:no-console
         console.debug("Request validated");
         const response = {
             "request_uuid": req.body.request_uuid,
@@ -26,22 +25,18 @@ app.post("/preprocessor", (req, res) => {
             }
         };
         if (ajv.validate("https://image.a11y.mcgill.ca/preprocessor-response.schema.json", response)) {
-            // tslint:disable-next-line:no-console
             console.debug("Valid response generated.");
             res.json(response);
         } else {
-            // tslint:disable-next-line:no-console
             console.debug("Failed to generate a valid response (did the schema change?)");
             res.status(500).send(ajv.errors);
         }
     } else {
-        // tslint:disable-next-line:no-console
         console.debug("Request did not pass the schema.");
         res.status(400).send(ajv.errors);
     }
 });
 
 app.listen(port, () => {
-    // tslint:disable-next-line:no-console
     console.log(`Started server on port ${port}`);
 });
