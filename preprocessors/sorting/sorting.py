58c3b7c960831d98c8d7700dbef1909c7a203174
rohanakut <rohan.akut97@gmail.com>
2022-01-12 14:50:06 -0500

    Added code for preprocessor

---
+++ preprocessors/sorting/sorting.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2021 IMAGE Project, Shared Reality Lab, McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# and our Additional Terms along with this program.
+# If not, see
+# <https://github.com/Shared-Reality-Lab/IMAGE-server/LICENSE>.
+
+from flask import Flask, request, jsonify
+import json
+import time
+import jsonschema
+import logging
+import collections
+from math import sqrt
+from operator import itemgetter
+
+
+app = Flask(__name__)
+
+
+def calculate_diagonal(x1, y1, x2, y2):
+    diag = sqrt((x2-x1)**2+(y2-y1)**2)
+    return diag
+
+
+@app.route("/preprocessor", methods=['POST', 'GET'])
+def readImage():
+    object_type = []
+    dimensions = []
+    area = []
+    left2right =[]
+    top2bottom =[]
+    small2big=[]
+    top_id = []
+    left_id = []
+    small_id = []
+    flag = 0
+    with open('./schemas/preprocessors/sorting.schema.json') as jsonfile:
+        data_schema = json.load(jsonfile)
+    with open('./schemas/preprocessor-response.schema.json') as jsonfile:
+        schema = json.load(jsonfile)
+    with open('./schemas/definitions.json') as jsonfile:
+        definitionSchema = json.load(jsonfile)
+    with open('./schemas/request.schema.json') as jsonfile:
+        first_schema = json.load(jsonfile)
+    # Following 6 lines of code are refered from
+    # https://stackoverflow.com/questions/42159346/jsonschema-refresolver-to-resolve-multiple-refs-in-python
+    schema_store = {
+        schema['$id']: schema,
+        definitionSchema['$id']: definitionSchema
+    }
+    resolver = jsonschema.RefResolver.from_schema(
+            schema, store=schema_store)
+    content = request.get_json()
+    try:
+        validator = jsonschema.Draft7Validator(first_schema, resolver=resolver)
+        validator.validate(content)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 400
+    preprocessor = content["preprocessors"]
+    if "ca.mcgill.a11y.image.preprocessor.objectDetection" \
+            not in preprocessor:
+        logging.info("Object detection output not "
+                     "available. Skipping...")
+        return "", 204
+    oDpreprocessor = \
+        preprocessor["ca.mcgill.a11y.image.preprocessor.objectDetection"]
+    objects = oDpreprocessor["objects"]
+    for i in range(len(objects)):
+        object_type.append(objects[i]["type"])
+        dimensions.append(objects[i]["dimensions"])
+        area.append(objects[i]["area"])
+    for i in range(len(objects)):
+        left2right.append([objects[i]["ID"],dimensions[i][2]])
+        top2bottom.append([objects[i]["ID"],dimensions[i][1]])
+        small2big.append([objects[i]["ID"],area[i]])
+    top2bottom = sorted(top2bottom, key=lambda x: x[1])
+    left2right = sorted(left2right, key=lambda x: x[1])
+    small2big = sorted(small2big, key=lambda x: x[1])
+    for i in range(len(top2bottom)):
+        top_id.append(top2bottom[i][0])
+        left_id.append(left2right[i][0])
+        small_id.append(small2big[i][0])
+    request_uuid = content["request_uuid"]
+    timestamp = time.time()
+    name = "ca.mcgill.a11y.image.preprocessor.sorting"
+    data = {"leftToRight": left_id, "topToBottom": top_id,"smallToBig":small_id}
+    try:
+        validator = jsonschema.Draft7Validator(data_schema)
+        validator.validate(data)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 500
+    response = {
+        "request_uuid": request_uuid,
+        "timestamp": int(timestamp),
+        "name": name,
+        "data": data
+        }
+    try:
+        validator = jsonschema.Draft7Validator(
+            schema, resolver=resolver)
+        validator.validate(response)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 500
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
