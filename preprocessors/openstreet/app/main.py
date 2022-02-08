1b78549fa56ad50835ab7081fb044eac0c30f6a7
Samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-02-08 18:34:25 +0100

    Modified the code to produce sets of points on the street

--- preprocessors/openstreet/app/main.py
+++ preprocessors/openstreet/app/main.py
@@ -1,6 +1,8 @@
 from typing import Optional
+from copy import deepcopy
 from fastapi import FastAPI
-from app.osm_service import query_osmdata, transform_osmdata,merge_street_by_name,extract_nodes_list,extract_intersection,merge_street_intersection_by_name
+from app.osm_service import query_osmdata, transform_osmdata,extract_nodes_list,extract_intersection
+from app.osm_service import create_new_intersection_sets, my_final_data_structure, merge_street_points_by_name,merge_street_by_name
 
 app = FastAPI()
 
@@ -11,11 +13,18 @@ def health():
 
 
 @app.get("/location/{radius}/{lat}/{lon}")
-def get_location(radius:float,lat:float, lon:float):
-  raw_osmdata=query_osmdata(radius, lat, lon)
-  transformed= transform_osmdata(raw_osmdata)
-  merged=merge_street_by_name(transformed)
-  result=extract_nodes_list(merged)
-  response=merge_street_intersection_by_name(result)
+def get_location(radius:float, lat:float, lon:float):
+  raw_osmdata = query_osmdata(radius, lat, lon)
+  transformed = transform_osmdata(raw_osmdata)
+  merged = merge_street_by_name(transformed)
+  result = extract_nodes_list(merged)
+  new_intersection_list = create_new_intersection_sets(result)
+
+  merged_d = deepcopy(merged)
+  modified_new_intersection_list = deepcopy(new_intersection_list)
   
-  return (response)
+  my_str_data = my_final_data_structure(merged_d,modified_new_intersection_list)
+  my_response = merge_street_points_by_name(my_str_data)
+  #response = merge_street_intersection_by_name(result)
+  
+  return (my_response)
--- preprocessors/openstreet/app/osm_service.py
+++ preprocessors/openstreet/app/osm_service.py
@@ -1,13 +1,15 @@
 from typing import List
 import overpy
 import json
-
+from copy import deepcopy
+#Send request to get map data from OSM
 def query_osmdata(radius:float, lat:float, lon:float):
   api = overpy.Overpass()
-  result=api.query (f'way(around:{radius},{lat},{lon})["highway"=""];(._;>;);out body;')
+  result = api.query (f'way(around:{radius},{lat},{lon})["highway"=""];(._;>;);out body;')
   return (result)
 
 
+#Retrieve inteterested street information from the requested OSM data
 def transform_osmdata(raw_osmdata: List[dict]):
   street_data_list=[]
   for way in raw_osmdata.ways:
@@ -18,15 +20,16 @@ def transform_osmdata(raw_osmdata: List[dict]):
         nodes_list.append(node_record)
         way_record={"street_name":way.tags.get("name","n/a"),"street_type":way.tags.get("highway","n/a"),"surface":way.tags.get("surface","n/a"),"oneway":way.tags.get("oneway","n/a"),"sidewalk":way.tags.get("sidewalk","n/a"),"nodes":nodes_list} 
         street_data_list.append(way_record)
-        #remove duplicate objects if any
+        #Remove duplicate objects if any
         cleaned_street_data_list=[]
         for obj in street_data_list:
-           if obj not in cleaned_street_data_list:
-             cleaned_street_data_list.append(obj)   
-               
+          if obj not in cleaned_street_data_list:
+            cleaned_street_data_list.append(obj)              
   return (cleaned_street_data_list)
 
-def merge_street_by_name(transformed_osmdata:List[dict]):
+
+#Group all street information on street name (to achieve street by street record)
+def merge_street_by_name(transformed_osmdata: List[dict]):
   response=transformed_osmdata
   output={}
   for obj in response:
@@ -47,80 +50,81 @@ def merge_street_by_name(transformed_osmdata:List[dict]):
       merged_street = list(output.values())
   return(merged_street)
 
+
+#Compare two streets for possible intersection
 def extract_intersection(list1, list2):
-  computed_intersection=[x for x in list1 if x in list2]  
+  computed_intersection = [x for x in list1 if x in list2]  
   return(computed_intersection)
 
 
+#The function uses loop to extract sets of points (nodes) for any two streets and send out for possible intersection and so on
 def extract_nodes_list(result):
-  intersection_sets=[]
+  intersection_sets = []
   for i in range(len(result)):
     for j in range(i + 1, len(result)):
-      list1=(result[i]["nodes"])
-      list2=(result[j]["nodes"])
-
-      intersection=extract_intersection(list1,list2)
+      list1 = (result[i]["nodes"])
+      list2 = (result[j]["nodes"])
+      intersection = extract_intersection(list1,list2)
       if len(intersection):
-        str_record={"street_name":result[i]["street_name"],"street_type":result[i]["street_type"],"surface":result[i]["surface"], "sidewalk":result[i]["sidewalk"],"oneway":result[i]["oneway"],"intersection_sets":intersection} 
-        intersection_sets.append(str_record)
-        str_record={"street_name":result[j]["street_name"],"street_type":result[j]["street_type"],"surface":result[j]["surface"], "sidewalk":result[j]["sidewalk"],"oneway":result[j]["oneway"],"intersection_sets":intersection} 
-        intersection_sets.append(str_record)
+        #str_record={"street_name":result[i]["street_name"],"street_type":result[i]["street_type"],"surface":result[i]["surface"], "sidewalk":result[i]["sidewalk"],"oneway":result[i]["oneway"],"intersection_sets":intersection} 
+        #intersection_sets.append(str_record)
+        #str_record={"street_name":result[j]["street_name"],"street_type":result[j]["street_type"],"surface":result[j]["surface"], "sidewalk":result[j]["sidewalk"],"oneway":result[j]["oneway"],"intersection_sets":intersection} 
+        intersection_sets.append(intersection)
   return(intersection_sets)
 
-def merge_street_intersection_by_name(response:List[dict]):
+def create_new_intersection_sets(result):
+  #Create a new single list of intersection objects [{},{},{}...]
+  modified_intersection_sets=[]
+  for intersect_list in result:
+    for obj in intersect_list:
+      modified_intersection_sets.append(obj)
+      
+  return(modified_intersection_sets)
+
+def my_final_data_structure(merged_street, modified_intersection_set):
+  str_records = []
+  for obj in range (len(merged_street)):
+    nodes= merged_street[obj]["nodes"]
+    for i in range (len(nodes)):
+      if nodes[i] not in modified_intersection_set:
+        alist=[]
+        node = nodes[i]
+        node["inter_link"] = "nil"
+        alist.append(node)
+        str_info={"street_name":merged_street[obj]["street_name"],"street_type":merged_street[obj]["street_type"],"surface":merged_street[obj]["surface"], "sidewalk":merged_street[obj]["sidewalk"],"oneway":merged_street[obj]["oneway"],"nodes":alist} 
+        str_records.append(str_info)
+      else:
+        alist=[]
+        node = nodes[i]
+        node["inter_link"] = "intersected"
+        alist.append(node)
+        str_info = {"street_name":merged_street[obj]["street_name"],"street_type":merged_street[obj]["street_type"],"surface":merged_street[obj]["surface"], "sidewalk":merged_street[obj]["sidewalk"],"oneway":merged_street[obj]["oneway"],"nodes":alist} 
+        str_records.append(str_info)
+  return(str_records)
+
+def merge_street_points_by_name(my_response):
   output={}
-  for obj in response:
+  for obj in my_response:
     str_name=obj["street_name"]
     if str_name not in output:
-      assert obj["intersection_sets"] is not None
-      record={"street_name":obj["street_name"],"street_type":obj["street_type"],"surface":obj["surface"], "sidewalk":obj["sidewalk"],"oneway":obj["oneway"],"intersection_sets": obj["intersection_sets"]} 
+      assert obj["nodes"] is not None
+      record={"street_name":obj["street_name"],"street_type":obj["street_type"],"surface":obj["surface"], "sidewalk":obj["sidewalk"],"oneway":obj["oneway"],"nodes": obj["nodes"]} 
       output[str_name]= record
     else:
       existing_record = output[str_name]
-      existing_intersection = existing_record["intersection_sets"]
-      assert existing_intersection is not None
-      new_intersection = obj["intersection_sets"]
-      assert new_intersection is not None
-      merged_intersection = existing_intersection + new_intersection
-      existing_record["intersection_sets"] = merged_intersection
+      existing_node = existing_record["nodes"]
+      assert existing_node is not None
+      new_node = obj["nodes"]
+      assert new_node is not None
+      merged_node = existing_node + new_node
+      existing_record["nodes"] = merged_node
       output[str_name] = existing_record
-      merged_intersection = list(output.values())
+      merged_node = list(output.values())
 
       #remove duplicate objects if any
-      cleaned_intersection_data_list=[]
-      for obj in merged_intersection:
-        if obj not in cleaned_intersection_data_list:
-          cleaned_intersection_data_list.append(obj)
-  return(cleaned_intersection_data_list)
-
-
-
-
-
-
-
-
-
-
-
-#def return_intersection(merged_street_data:List[dict]):
- # street_intersection=[]
-  #for i,items in enumerate (merged_street_data):
-   # nodes_object=items["nodes"]
-    #intersection=process_intersection(nodes_object)
-    #total_coordinates_sets=len(intersection)
-    #intersection_records={"street_name":items["street_name"],"street_type":items["street_type"],"surface":items["surface"], "sidewalk":items["sidewalk"],"oneway":items["oneway"],"total_coordinates_sets":total_coordinates_sets,"intersection_coordinates":intersection}
-    #street_intersection.append(intersection_records)
-  #return(street_intersection)
-
-#def process_intersection(nodes_object):
-  #intersection_list=[]
-  #for i, items in enumerate (nodes_object):
-   # number_of_nodes=len(nodes_object)
-    #if i<number_of_nodes:
-     # nom=i+1
-     # for j in range (nom,number_of_nodes):
-      #  if nodes_object[i]==nodes_object[j]:
-       #   shared_nodes=nodes_object[i]
-        #  intersection_list.append(shared_nodes)
-  #return(intersection_list)
+      #cleaned_node_data_list=[]
+      #for obj in merged_node:
+      #  if obj not in cleaned_node_data_list:
+         # cleaned_node_data_list.append(obj)
+         # merged_data_structure=cleaned_node_data_list
+  return(merged_node)
