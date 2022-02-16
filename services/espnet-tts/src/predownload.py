6e098e4123ca725a9f7bc8e6586dccd1f56fcb33
Juliette Regimbal <juliette.regimbal@mailbox.org>
2022-02-16 15:38:35 -0500

    Bump parallel_wavegan version, download on build
    Downloading on run can lead to #353 when we hit Google's rate limiting.
    So we are setting melgan to be the default downloaded on build.

--- services/espnet-tts/src/predownload.py
+++ services/espnet-tts/src/predownload.py
@@ -19,4 +19,4 @@ from parallel_wavegan.utils import download_pretrained_model
 
 d = ModelDownloader()
 d.download_and_unpack("kan-bayashi/ljspeech_conformer_fastspeech2")
-download_pretrained_model("ljspeech_parallel_wavegan.v1")
+download_pretrained_model("ljspeech_full_band_melgan.v2")
