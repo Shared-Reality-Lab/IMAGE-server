8c55e253749e62c20e45b658afb54ce46596042c
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2022-07-26 23:36:28 +0100

    Modified file

--- preprocessors/openstreetmap/osm_service.py
+++ preprocessors/openstreetmap/osm_service.py
@@ -44,7 +44,7 @@ def get_streets(bbox_coord):
             url="https://pegasus.cim.mcgill.ca/overpass/api/interpreter?")
         OSM_data = api.query(
             f"""
-        way({lat_min},{lon_min},{lat_max},{lon_max})[highway~"^(primary|tertiary|secondary|track|path|living_street|service|crossing|pedestrian|residential|footway)$"];
+        way({lat_min},{lon_min},{lat_max},{lon_max})[highway];
         (._;>;);
         out body;
         """
