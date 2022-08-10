73323a6a45bd2917c10795f960ea8e266aaf6640
jaydeepsingh25 <jaydeepsingh_25@yahoo.com>
2022-08-10 15:44:59 -0700

    fixes

--- preprocessors/charts/charts_utils.py
+++ preprocessors/charts/charts_utils.py
@@ -11,6 +11,7 @@ def getLowerPointsOnLeft(index, point, series_data):
             break
     return count
 
+
 # For a given point at a index, the function returns the number of
 # contiguous points(on the left side of the point) higher than the point value
 def getHigherPointsOnLeft(index, point, series_data):
@@ -23,6 +24,7 @@ def getHigherPointsOnLeft(index, point, series_data):
         else:
             break
     return count
+
 
 # For a given point at a index, the function returns the number of
 # contiguous points(on the right side of the point) lower than the point value
@@ -37,6 +39,7 @@ def getLowerPointsOnRight(index, point, series_data):
             break
     return count
 
+
 # For a given point at a index, the function returns the number of
 # contiguous points(on the right side of the point) higher than the point value
 def getHigherPointsOnRight(index, point, series_data):
@@ -49,4 +52,3 @@ def getHigherPointsOnRight(index, point, series_data):
         else:
             break
     return count
-
