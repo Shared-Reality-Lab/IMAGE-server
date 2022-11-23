b6748f18b6459d065446ec335df6d93f00657863
dailun li <dailun.li@mail.mcgill.ca>
2022-11-23 13:22:58 -0500

    TODO vocoder in unsupported format

--- services/espnet-tts/src/espnet_util.py
+++ services/espnet-tts/src/espnet_util.py
@@ -25,41 +25,61 @@ from parallel_wavegan.utils import download_pretrained_model
 from parallel_wavegan.utils import load_model
 
 fs = 22050
-tag = "kan-bayashi/ljspeech_conformer_fastspeech2"
+
+# tag = "kan-bayashi/ljspeech_conformer_fastspeech2"
 # vocoder_tag = "ljspeech_parallel_wavegan.v3"
+
 vocoder_tag = "ljspeech_full_band_melgan.v2"
+
+
 
 logging.basicConfig(format="%(asctime)s %(message)s")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+## place the model here
+## as the model downloaded 
+
+tag ="/home/python/.cache/models/siwis-tacotron-300epoch.pth"
+
 d = ModelDownloader()
 device = environ["TORCH_DEVICE"]
 logger.info(f"Device: {device}")
-text2speech = Text2Speech(
-    **d.download_and_unpack(tag),
-    device=device,
-    threshold=0.5,
-    minlenratio=0.0,
-    maxlenratio=10.0,
-    use_att_constraint=False,
-    backward_window=1,
-    forward_window=3,
-    # Only for FastSpeech & FastSpeech2
-    speed_control_alpha=1.0,
+
+
+# text2speech = Text2Speech(
+#     **d.download_and_unpack(tag),
+#     # model_file=tag,
+#     device=device,
+#     threshold=0.5,
+#     minlenratio=0.0,
+#     maxlenratio=10.0,
+#     use_att_constraint=False,
+#     backward_window=1,
+#     forward_window=3,
+#     # Only for FastSpeech & FastSpeech2
+#     speed_control_alpha=1.0
+# )
+
+text2speech = Text2Speech.from_pretrained(
+    model_file=tag,
+    # device=device
+    vocoder_tag=vocoder_tag
 )
-text2speech.spc2wav = None
-vocoder = load_model(download_pretrained_model(vocoder_tag)).to(device).eval()
-vocoder.remove_weight_norm()
+
+# text2speech.spc2wav = None
+# vocoder = load_model(download_pretrained_model(vocoder_tag)).to(device).eval()
+# vocoder.remove_weight_norm()
 
 
 @lru_cache()
 def tts(text):
     with torch.no_grad():
         start = time.time()
-        wav, c, *_ = text2speech(text)
+        wav = text2speech(text)["wav"]
+        # wav, c, *_ = text2speech(text)
         t2 = time.time()
-        wav = vocoder.inference(c)
+        # wav = vocoder.inference(c)
         t3 = time.time()
     rtf = (t3 - start) / (len(wav) / fs)
     logger.info(f"RTF: {rtf}")
