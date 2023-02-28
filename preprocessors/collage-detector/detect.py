3145d52ab3286f51e868274208ed983fb8fc7932
jaydeepsingh25 <jaydeepsingh_25@yahoo.com>
2023-02-27 17:26:02 -0800

    fix: add license info

--- preprocessors/collage-detector/detect.py
+++ preprocessors/collage-detector/detect.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2023 IMAGE Project, Shared Reality Lab, McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# and our Additional Terms along with this program.
+# If not, see
+# <https://github.com/Shared-Reality-Lab/IMAGE-server/blob/main/LICENSE>.
+
 from SBRIF import SbRIF
 from flask import Flask, request, jsonify
 import numpy as np
