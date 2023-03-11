57a97bc8981e7e31f257c2248435adcafd65578f
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2023-03-11 15:57:33 -0500

    changes for accordance to PEP8
    Added another blank space on line 164

--- preprocessors/depth-map-gen/depth-map-generator.py
+++ preprocessors/depth-map-gen/depth-map-generator.py
@@ -161,6 +161,7 @@ def depthgenerator():
     logging.debug("Sending response")
     return response
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
     depthgenerator()
