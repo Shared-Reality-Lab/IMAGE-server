370519b34e3e050b3b21d72a25f5c73dafe8f7b2
Romain Bazin <romain.bzin@gmail.com>
2023-03-21 09:02:46 -0400

    removing main artifact function

--- preprocessors/mmsemseg/segment.py
+++ preprocessors/mmsemseg/segment.py
@@ -31,60 +31,6 @@ CLASS_NAMES = mmseg.core.evaluation.get_classes("ade20k")
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-def main(config_file, checkpoint_file, image_folder, output_folder):
-    # empty gpu cache
-    torch.cuda.empty_cache()
-
-    # create output folder
-    os.makedirs(output_folder, exist_ok=True)
-
-    # init logger
-    logging.basicConfig(filename=os.path.join(output_folder, "log.txt"), level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
-    logging.info(f"Starting segmentation with config file {config_file} and checkpoint file {checkpoint_file}.")
-
-
-    # build the model from a config file and a checkpoint file
-    model = init_segmentor(config_file, checkpoint_file, device='cuda:1') # TODO : change the device to cuda:0 after changing the container gpu to 1
-
-    # test a single image and show the results
-    img_files = [os.path.join(image_folder, image_file) for image_file in os.listdir(image_folder)]
-
-    for img_file in img_files:
-        # load image with opneCV
-        img = mmcv.imread(img_file)
-
-        # rescale the image 
-        img = mmcv.imrescale(img, 0.5) # TODO : rescale based on the size of the image
-
-        # infer the segmentation
-        start = time()
-        result = inference_segmentor(model, img)
-        print(result)
-        print(f"result length: {len(result)}")
-        print(f"Segmentation shape: {result[0].shape}")
-        end = time()
-
-        pred = result[0].astype(np.uint8)
-        
-        predicted_classes = np.bincount(pred.flatten()).argsort()[::-1]
-        for class_id in predicted_classes[:5]:
-            print(f"Class id: {class_id}")
-            print(f"Class name: {CLASS_NAMES[class_id]}")
-        
-        pred_color = visualize_result(img, pred, index = predicted_classes[0])
-        print(f"pred_color shape: {pred_color.shape}")
-
-        # logs the time it took to segment the image
-        img_name = os.path.basename(img_file).split(".")[0]
-        logging.info(f"Segmented {img_name} in {end - start:.2f} seconds. Classes found: {[class_name for class_name, _ in predicted_classes]}")
-
-        # save image with overlapping segmentation
-        model.show_result(img, result, out_file=os.path.join(output_folder, f"{img_name}_segmented.png"), opacity=0.7)
-    
-    # empty gpu cache
-    torch.cuda.empty_cache()
-
-
 def run_segmentation(url, model, dictionnary):
     # convert an image from base64 format
     # Following 4 lines refered from
@@ -117,7 +63,7 @@ def run_segmentation(url, model, dictionnary):
         result = inference_segmentor(model, image_np)
     except Exception as e:
         logging.error("error while running segmentation model : {}".format(e))
-        
+
     
     logging.info("run finished")
 
