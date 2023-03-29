8b89c8122b19e7df00257d085650ab6f7f5e859c
Romain Bazin <romain.bzin@gmail.com>
2023-03-29 17:20:27 -0400

    top comment

--- preprocessors/mmsemseg/segment.py
+++ preprocessors/mmsemseg/segment.py
@@ -1,4 +1,20 @@
-""" Semantic segmentation preprocessor """
+# Copyright (c) 2023 IMAGE Project, Shared Reality Lab, McGill University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# and our Additional Terms along with this program.
+# If not, see
+# <https://github.com/Shared-Reality-Lab/IMAGE-server/blob/main/LICENSE>.
+#
+# This was built using the MMsegmentation library that can be found at https://github.com/open-mmlab/mmsegmentation
 
 from flask import Flask, request, jsonify
 import gc
