642d5362c4ce3936815a524824b41239f0f681cd
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2023-04-06 12:59:58 -0400

    Create object-depth-calculator.py

---
+++ preprocessors/object-depth-calculator/object-depth-calculator.py
@@ -0,0 +1,90 @@
+import cv2
+import argparse
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from flask import Flask, request, jsonify
+from collections import OrderedDict
+import json
+import time
+import jsonschema
+import logging
+import base64
+
+app = Flask(__name__)
+
+@app.route("/preprocessor", methods=['POST', ])
+def objectdepth():
+    logging.debug("Received request")
+    # load the schema
+    with open('./schemas/preprocessors/object-depth-calculator.schema.json') \
+            as jsonfile:
+        data_schema = json.load(jsonfile)
+    with open('./schemas/preprocessor-response.schema.json') \
+            as jsonfile:
+        schema = json.load(jsonfile)
+    with open('./schemas/definitions.json') as jsonfile:
+        definitionSchema = json.load(jsonfile)
+    with open('./schemas/request.schema.json') as jsonfile:
+        first_schema = json.load(jsonfile)
+    # Following 6 lines of code
+    # refered from
+    # https://stackoverflow.com/questions/42159346/jsonschema-refresolver-to-resolve-multiple-refs-in-python
+    schema_store = {
+        schema['$id']: schema,
+        definitionSchema['$id']: definitionSchema
+    }
+    resolver = jsonschema.RefResolver.from_schema(
+        schema, store=schema_store)
+    content = request.get_json()
+    try:
+        validator = jsonschema.Draft7Validator(first_schema, resolver=resolver)
+        validator.validate(content)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 400
+    # check for image
+    if "graphic" not in content:
+        logging.info("Request is not a graphic. Skipping...")
+        return "", 204  # No content
+    request_uuid = content["request_uuid"]
+    timestamp = time.time()
+    name = "ca.mcgill.a11y.image.preprocessor.object-depth-calculator"
+
+    # convert the uri to processable image
+    # Following 4 lines of code
+    # refered form
+    # https://gist.github.com/daino3/b671b2d171b3948692887e4c484caf47
+    source = content["graphic"]
+    image_b64 = source.split(",")[1]
+    binary = base64.b64decode(image_b64)
+    image = np.asarray(bytearray(binary), dtype="uint8")
+    img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    
+    
+    try:
+        validator = jsonschema.Draft7Validator(data_schema)
+        validator.validate(depth)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 500
+    response = {
+        "request_uuid": request_uuid,
+        "timestamp": int(timestamp),
+        "name": name,
+        "data": depth
+    }
+    try:
+        validator = jsonschema.Draft7Validator(schema, resolver=resolver)
+        validator.validate(response)
+    except jsonschema.exceptions.ValidationError as e:
+        logging.error(e)
+        return jsonify("Invalid Preprocessor JSON format"), 500
+    torch.cuda.empty_cache()
+    logging.debug("Sending response")
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    objectdepth()
