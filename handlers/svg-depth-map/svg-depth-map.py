a5a46ba236b39e0faf9db64d6f42a6d4c1407992
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2023-04-10 11:30:15 -0400

    PEP8 corrections
    got rid of unused import and formatting

--- handlers/svg-depth-map/svg-depth-map.py
+++ handlers/svg-depth-map/svg-depth-map.py
@@ -21,7 +21,6 @@ from jsonschema.exceptions import ValidationError
 import logging
 import time
 import drawSvg as draw
-import random
 import svgwrite
 import base64
 
@@ -126,11 +125,11 @@ def handle():
 
     d = preprocessors["ca.mcgill.a11y.image.preprocessor.depth-map-gen"]
     depth = d["depth-map"]
-    svg = svgwrite.Drawing("depth.svg",(dimensions[0],dimensions[1]))
+    svg = svgwrite.Drawing("depth.svg", (dimensions[0], dimensions[1]))
     svg_layers = []
     
     if (len(depth) > 0):
-      svg.add(svg.image(href=(depth), size=(dimensions[0],dimensions[1])))
+      svg.add(svg.image(href=(depth), size=(dimensions[0], dimensions[1])))
       base64_bytes = base64.b64encode(svg.tostring().encode("utf-8"))
       sendData = "data:image/svg+xml;base64," + base64_bytes.decode("utf-8")
       svg_layers.append({"label": "Graphic Depth map", "svg": sendData})
