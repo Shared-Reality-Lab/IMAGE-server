d17b1405aaa857bac8088cc6971e5c898bd88055
Venissa Carol Quadros <venicq@gmail.com>
2023-06-01 20:01:08 -0400

    Added missing check

--- handlers/photo-tactile-svg/tactile_svg.py
+++ handlers/photo-tactile-svg/tactile_svg.py
@@ -130,7 +130,8 @@ def handle():
     # or semantic segmentation is present
     svg = draw.Drawing(dimensions[0], dimensions[1])
 
-    if "ca.mcgill.a11y.image.preprocessor.objectDetection" in preprocessors:
+    if "ca.mcgill.a11y.image.preprocessor.objectDetection"\
+        and "ca.mcgill.a11y.image.preprocessor.grouping" in preprocessors:
         preprocessor_names.append('Things and people')
         o = preprocessors["ca.mcgill.a11y.image.preprocessor.objectDetection"]
         g = preprocessors["ca.mcgill.a11y.image.preprocessor.grouping"]
