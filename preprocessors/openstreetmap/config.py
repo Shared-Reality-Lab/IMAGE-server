71504cc3e897c6a3e7c88e768354b8286b7a7e89
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2023-07-14 19:27:18 +0100

    Assign variable names to servers

--- preprocessors/openstreetmap/config.py
+++ preprocessors/openstreetmap/config.py
@@ -8,3 +8,10 @@ defaultServer = "https://pegasus.cim.mcgill.ca/overpass/api/interpreter?"
 
 secondaryServer1 = "https://maps.mail.ru/osm/tools/overpass/api/interpreter"
 secondaryServer2 = "https://overpass-api.de/api/interpreter"
+
+
+# Set names for map servers
+
+map_server_1 = "Pegasus Server"
+map_server_2 = "VK Maps Overpass API Server"
+map_server_3 = "Main Overpass Server"
