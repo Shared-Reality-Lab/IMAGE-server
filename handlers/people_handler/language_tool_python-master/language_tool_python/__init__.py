b2019854e8e69881772b74e4a3c049d447d7d02b
rohanakut <rohan.akut97@gmail.com>
2023-09-01 19:39:32 -0400

    Deleted language tools

--- handlers/people_handler/language_tool_python-master/language_tool_python/__init__.py
+++
@@ -1,9 +0,0 @@
-"""LanguageTool through server mode.
-
-    migration URL: https://languagetool.org/http-api/migration.php
-"""
-
-from .language_tag import LanguageTag
-from .match import Match
-from .server import LanguageTool, LanguageToolPublicAPI
-from . import utils
