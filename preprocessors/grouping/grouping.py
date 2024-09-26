af29af0b7e2343b05a15cb2a8c8bb6bf3b64505f
Shahd Yousef <shahdyousefak@gmail.com>
2024-09-25 22:57:51 -0400

    Added 1 healthcheck to grouping preprocessor. Health check verifies its status every 30 secs

--- preprocessors/grouping/grouping.py
+++ preprocessors/grouping/grouping.py
@@ -135,6 +135,9 @@ def readImage():
     logging.debug("Sending response")
     return response
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "healthy"}, 200)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
