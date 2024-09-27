edde996b5802ca2185773e8750781d395996584d
Shahd Yousef <shahdyousefak@gmail.com>
2024-09-27 10:10:03 -0400

    Modifying the script to use slack webhook env variable, and cleaning some logic

--- preprocessors/healthcheck.sh
+++ preprocessors/healthcheck.sh
@@ -7,7 +7,11 @@ fi
 
 CONTAINER_NAME=$1
 
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
+SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL:-""}
+if [ -z "$SLACK_WEBHOOK_URL" ]; then
+  echo "Slack webhook URL is not configured correctly. Exiting."
+  exit 1
+fi
 
 # check the health status of the container
 health_status=$(docker inspect --format='{{json .State.Health.Status}}' "$CONTAINER_NAME" 2>/dev/null)
@@ -19,17 +23,17 @@ fi
 
 # Notify via Slack if the container is not healthy (Will remove the healthy once tested)
 if [ "$health_status" != "\"healthy\"" ]; then
-    response=$(curl -X POST --data-urlencode "payload={\"channel\": \"#preprocessors\", \"username\": \"docker-health-check\", \"text\": \"Container *$CONTAINER_NAME* is healthy.\"}" $SLACK_WEBHOOK_URL 2>/dev/null)
-
-    if [ "$response" == "ok" ]; then
-        echo "Message successfully posted to Slack."
-    else
-        echo "Failed to post message to Slack: $response"
-    fi
-    
+    message="Container *$CONTAINER_NAME* health check failed with status: *$health_status*"
 else
-  curl -X POST --data-urlencode "payload={\"channel\": \"#preprocessors\", \"username\": \"docker-health-check\", \"text\": \"Container *$CONTAINER_NAME* is healthy.\"}" $SLACK_WEBHOOK_URL
+    message="Container *$CONTAINER_NAME* is healthy."
 fi
 
+response=$(curl -X POST --data-urlencode "payload={\"channel\": \"#preprocessors\", \"username\": \"docker-health-check\", \"text\": \"$message\"}" $SLACK_WEBHOOK_URL 2>/dev/null)
 
-## how could tjos fial?
+if [ "$response" == "ok" ]; then
+    echo "Message successfully posted to Slack."
+else
+    echo "Failed to post message to Slack: $response"
+fi
+
+## how could this fail?
