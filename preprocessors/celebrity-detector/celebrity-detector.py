1a807382796698e94e91373fee8ff5d990a834d4
Shahd Yousef <shahdyousefak@gmail.com>
2024-10-11 23:25:32 -0400

    fixing spacing

--- preprocessors/celebrity-detector/celebrity-detector.py
+++ preprocessors/celebrity-detector/celebrity-detector.py
@@ -218,12 +218,14 @@ def categorise():
                       "celebrities out of " + str(len(objects)) + "objects")
         return response
 
+
 @app.route('/health', methods=['GET'])
 def health():
     """
     health check endpoint to verify if the service is up.
     """
     return jsonify({"status": "healthy"}), 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
