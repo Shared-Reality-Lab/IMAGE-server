a9cc11b7dfef3a4f1cc99af21875eacbb77fdff1
Jaydeep Singh <jaydeepsingh_25@yahoo.com>
2025-09-27 04:44:14 +0530

    fix: content categoriser updates (#1156)

--- preprocessors/mmsemseg/segment.py
+++ preprocessors/mmsemseg/segment.py
@@ -161,8 +161,9 @@ def segment():
     # if the architecture is modified appropriately
     if classifier_1 in preprocess_output:
         classifier_1_output = preprocess_output[classifier_1]
-        classifier_1_label = classifier_1_output["category"]
-        if classifier_1_label != "photograph":
+        categoriser_tags = classifier_1_output["categories"]
+
+        if not categoriser_tags["photo"]:
             logging.info("Not photograph content. Skipping...")
             return "", 204
         if classifier_2 in preprocess_output:
